d932243c466e69547c3cf02984e9f1e21427e027
Jamie <jr-ingram@hotmail.co.uk>
2018-02-23 15:49:30 +0000

    Can convert strings to jira tables

--- csvtojira.py
+++ csvtojira.py
@@ -1,14 +1,26 @@
 import csv
 
 def convertStringToJiraRow(csvRow):
-    convertedRow = "|" + csvRow.replace(",",  "|") + "|"
+    convertedRow = "|" + csvRow.replace(",",  "|") + "|\n"
     return convertedRow
     
 def convertStringToJiraHeader(csvRow):
-    convertedRow = "||" + csvRow.replace(",",  "||") + "||"
-    return convertedRow
+    convertedRow = "||" + csvRow.replace(",",  "||") + "||\n"
+    return convertedRow 
 
 
+def convertCSVStringToJira(CSVString):
+    stringArray = CSVString.splitlines()
+    convertedString = ""
+    firstRow = True
+    for line in stringArray:
+        if(firstRow):
+            convertedString = convertStringToJiraHeader(line)
+            firstRow = False
+        else:
+            convertedString = convertedString + convertStringToJiraRow(line)
+     
+    print(convertedString)
 def convertCSVFileToJira():
     with open("test.csv", 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -17,12 +29,12 @@ def convertCSVFileToJira():
         for row in reader:
             if(firstRow):
                 originalString = row[0] + ',' + row[1] + ',' + row[2]
-                convertedString = convertStringToJiraHeader(originalString) + "\n"
+                convertedString = convertStringToJiraHeader(originalString)
                 firstRow = False
                 
             else:
                 originalString = row[0] + ',' + row[1] + ',' + row[2]
-                convertedString = convertedString + convertStringToJiraRow(originalString) + "\n"
+                convertedString = convertedString + convertStringToJiraRow(originalString)
                 
         print(convertedString)
         file = open("test.txt",  "w")
@@ -30,4 +42,6 @@ def convertCSVFileToJira():
         file.close()
 
 convertCSVFileToJira()
+print("*********************************************************************")
+convertCSVStringToJira("letter,food,healthy\na,apple,Y\nb,banana,Y\nc,chocolate,N\nd,dried fruit,Y")
 print("Conversion completed!")

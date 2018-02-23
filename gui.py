df65d292754cafcb0eb93120652169397df659c8
Jamie <jr-ingram@hotmail.co.uk>
2018-02-23 15:19:50 +0000

    Button now activated when radio button is selected

--- gui.py
+++ gui.py
@@ -1,27 +1,31 @@
 import tkinter
 
-def activateConvertButton(conversion):
-    if conversion == "File":
-        print("FILE")
-    elif type == "Text":
-        print("TEXT")
+def activateConvertButton(button):
+   button.config(state="normal")
 
 def createGUI():
     window = tkinter.Tk()
     window.title("CSV To Jira")
 
     #The button which when pressed converts text.
-    convertButton = tkinter.Button(window,  text="Convert",  state="disabled").grid(row=2,  column=3)
+    #Set to a disabled state to ensure an option is selected by the radio buttons.
+    convertButton = tkinter.Button(window,  text="Convert",  state="disabled")
+    convertButton.grid(row=2,  column=2)
     
 
-
-    conversionFromLabel = tkinter.Label(window,  text="Convert from a CSV File or Text Input Below?:").grid(row=0)
     conversionFrom = ""
-    textEnteredRadio = tkinter.Radiobutton(window, text="Text Entered Below", variable=conversionFrom, value="Text").grid(row=0,  column=1)
-    csvFileRadio = tkinter.Radiobutton(window, text="CSV File", variable=conversionFrom, value="File", command=lambda: activateConvertButton("File")).grid(row=0,  column=2)
+    conversionFromLabel = tkinter.Label(window,  text="Convert from a CSV File or Text Input Below?:")
+    conversionFromLabel.grid(row=0)
+    
+    #Creates two radio buttons for options on what to convert from.
+    textEnteredRadio = tkinter.Radiobutton(window, text="Text Entered Below", variable=conversionFrom, value="Text", command=lambda: activateConvertButton(convertButton))
+    textEnteredRadio.grid(row=0,  column=1)
+    csvFileRadio = tkinter.Radiobutton(window, text="CSV File", variable=conversionFrom, value="File", command=lambda: activateConvertButton(convertButton))
+    csvFileRadio.grid(row=0,  column=2)
 
 
-    textArea = tkinter.Text(window, width=80, height=30).grid(row=1, columnspan=3)
+    textArea = tkinter.Text(window, width=80, height=30)
+    textArea.grid(row=1, columnspan=4)
 
     window.mainloop()
 
